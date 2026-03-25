2295449e9cae4d8714f690fbebdc6f3c593a4f23
Dimon2075 <dmitriy.gusakov.2006@mail.ru>
2026-03-25 23:28:53 +0300

    Коментарии

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
-﻿int result = Fibonacci(5);
+﻿/// <summary>
+/// Основная программа для вычисления числа Фибоначчи.
+/// </summary>
+int result = Fibonacci(5);
 Console.WriteLine(result);
 
+/// <summary>
+/// Вычисляет число Фибоначчи для заданного индекса <paramref name="n"/>.
+/// </summary>
+/// <param name="n">Индекс (нумерация с нуля) в последовательности Фибоначчи.</param>
+/// <returns>Число Фибоначчи на позиции <paramref name="n"/>.</returns>
 static int Fibonacci(int n)
 {
-    Console.WriteLine("The output is: ");
-    int n1 = 0;
-    int n2 = 1;
+    Console.WriteLine("Выводится результат: ");
+    int n1 = 0; // первое число Фибоначчи
+    int n2 = 1; // второе число Фибоначчи
     int sum;
 
     for (int i = 2; i <= n; i++)
@@ -15,5 +23,6 @@ static int Fibonacci(int n)
         n2 = sum;
     }
 
+    // Возвращаем нужное число Фибоначчи в зависимости от n
     return n == 0 ? n1 : n2;
 }
