e1ddc1a8d3d284c33df11b4d07521563ea4d44c1
Dimon2075 <dmitriy.gusakov.2006@mail.ru>
2026-03-25 22:39:57 +0300

    Новый код

--- Program.cs
+++ Program.cs
@@ -1,19 +1,113 @@
-﻿int result = Fibonacci(5);
-Console.WriteLine(result);
+﻿using System;
+using System.Collections.Generic;
 
-static int Fibonacci(int n)
+namespace ConsoleApp_FirstApp
 {
-    Console.WriteLine("The output is: ");
-    int n1 = 0;
-    int n2 = 1;
-    int sum;
-
-    for (int i = 2; i <= n; i++)
+    /// <summary>
+    /// Основная программа, которая запускается при запуске приложения.
+    /// </summary>
+    class Program
     {
-        sum = n1 + n2;
-        n1 = n2;
-        n2 = sum;
+        /// <summary>
+        /// Главный входной метод программы.
+        /// </summary>
+        /// <param name="args">Массив строковых аргументов командной строки.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Galaxy News!");
+            IterateThroughList();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Метод для итерации по списку галактик и вывода информации о них.
+        /// </summary>
+        private static void IterateThroughList()
+        {
+            var theGalaxies = new List<Galaxy>
+            {
+                new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
+                new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
+                new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
+                new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
+                new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
+                new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
+            };
+
+            foreach (Galaxy theGalaxy in theGalaxies)
+            {
+                Console.WriteLine(theGalaxy.Name + "  " + theGalaxy.MegaLightYears + ",  " + theGalaxy.GalaxyType.MyGType);
+            }
+
+            // Ожидаемый вывод:
+            //  Tadpole  400,  Spiral
+            //  Pinwheel  25,  Spiral
+            //  Cartwheel, 500,  Lenticular
+            //  Small Magellanic Cloud .2,  Irregular
+            //  Andromeda  3,  Spiral
+            //  Maffei 1,  11,  Elliptical
+        }
     }
 
-    return n == 0 ? n1 : n2;
+    /// <summary>
+    /// Класс, представляющий галактику.
+    /// </summary>
+    public class Galaxy
+    {
+        /// <summary>
+        /// Название галактики.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Расстояние до галактики в миллионах световых лет.
+        /// </summary>
+        public double MegaLightYears { get; set; }
+
+        /// <summary>
+        /// Тип галактики.
+        /// </summary>
+        public GType GalaxyType { get; set; }
+    }
+
+    /// <summary>
+    /// Класс, представляющий тип галактики.
+    /// </summary>
+    public class GType
+    {
+        /// <summary>
+        /// Создает новый тип галактики на основе символьного кода.
+        /// </summary>
+        /// <param name="type">Символьный код типа: 'S' (спираль), 'E' (эллиптическая), 'I' (неправильная), 'L' (ланцетная).</param>
+        public GType(char type)
+        {
+            switch (type)
+            {
+                case 'S':
+                    MyGType = Type.Spiral;
+                    break;
+                case 'E':
+                    MyGType = Type.Elliptical;
+                    break;
+                case 'I':
+                    MyGType = Type.Irregular;
+                    break;
+                case 'L':
+                    MyGType = Type.Lenticular;
+                    break;
+                default:
+                    throw new ArgumentException("Неверный код типа галактики");
+            }
+        }
+
+        /// <summary>
+        /// Свойство, хранящее тип галактики.
+        /// </summary>
+        public object MyGType { get; set; }
+
+        /// <summary>
+        /// Перечисление, определяющее возможные типы галактик.
+        /// </summary>
+        private enum Type { Spiral, Elliptical, Irregular, Lenticular }
+    }
 }
